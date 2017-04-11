5ead7a179456644966ddf172de8294d9b7152246
genobayashi <juventus1025@gmail.com>
2017-04-11 19:12:01 +0900

    Crrect ssh key

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['C:\Users\Owner\Downloads\キー\juve1897.pem']
+                  keys: ['/home/vagrant/.ssh/juve1897.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
