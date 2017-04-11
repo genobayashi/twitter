5e12f5ca04526d0f8c5bd79dfd861ca2014ebdc3
genobayashi <juventus1025@gmail.com>
2017-04-11 18:43:54 +0900

    Crrect elastic ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '<Elastic IP>', user: 'ec2-user', roles: %w{app db web}
+server '52.198.114.99', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
