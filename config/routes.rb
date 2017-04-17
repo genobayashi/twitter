9a2d9d95c747e17ebcddb7b4df34e56e95480b71
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-04-18 01:18:49 +0900

    routing edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :top, only: [:index]
   resources :tweets, only: [:index, :create]
 end
